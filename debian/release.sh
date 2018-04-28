de7a31bd03f69f42cb6e6db522094577188ef608
David Lechner <david@lechnology.com>
2018-04-27 22:10:42 -0500

    debian: add raspbian to release script

--- debian/release.sh
+++ debian/release.sh
@@ -10,9 +10,12 @@ distribution=$(dpkg-parsechangelog -S Distribution)
 codename=$(debian-distro-info --codename --${distribution})
 
 OS=debian DIST=${codename} ARCH=amd64 pbuilder-ev3dev build
+OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
 
 debsign ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+debsign ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
 
 dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-amd64/${source}_${version}_amd64.changes
+dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
 
 gbp buildpackage --git-tag-only
